103212faad871a780315a1ea9cd97d77a888dfa7
Richard <r.m.hill@ucl.ac.uk>
2023-10-04 09:20:29 +0100

    changed imports in not_false

--- UCLMATH0109/Course/_2_foundations/C_not_false.lean
+++ UCLMATH0109/Course/_2_foundations/C_not_false.lean
@@ -1,4 +1,5 @@
-import Mathlib.Tactic
+import Std.Tactic.RCases
+import Std.Tactic.Basic
 
 /-
 # Not : ¬ P  (tactics: contradiction / intro / apply / exfalso / by_cases)
@@ -21,8 +22,9 @@ We can start a proof of `¬ P` with `intro hp` and then we try to derive a contr
 example : ¬ (P ∧ ¬P):=
 by
   intro h
-  obtain ⟨hp,hnp⟩:= h 
+  obtain ⟨hp,hnp⟩:= h
   contradiction
+
 
 /-
 Since **any** proposition `Q` follows from a contradiction we can always replace any goal
@@ -30,16 +32,16 @@ Since **any** proposition `Q` follows from a contradiction we can always replace
 -/
 example (hf: False ) : Q :=
 by
-  -- Change the goal from `⊢ P` to `⊢ False`
+-- Change the goal from `⊢ P` to `⊢ False`
   exfalso
-  -- Complete the proof using `hf : False`  
+-- Complete the proof using `hf : False`  
   exact hf
 
--- In our next example we could start with `intro hp, intro hn`
--- but Lean has a convenient `intros` tactic that combines the two
+-- In our next example we could start with `intro hp`, `intro hn`
+-- but these two tactics can me merged into one.
 example : P → ¬¬P :=  
 by
-  intros hp hn 
+  intros hp hn
   contradiction
 
 
@@ -55,7 +57,7 @@ by
   intro hnp
   by_cases hp : P
   · exact hp
--- We have `hp: ¬P` and `hnp: ¬¬P` in the context which is a contradiction 
+-- We have `hp: ¬P` and `hnp: ¬¬P` in the context which is a contradiction
   · contradiction
 
 
