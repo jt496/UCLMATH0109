f9d32bedbf400733047f7cee61086ff41997c727
jt496 <john.talbot@gmail.com>
2023-10-04 08:53:01 +0100

    sheet D ready

--- UCLMATH0109/Course/_2_foundations/sheet2D.lean
+++ UCLMATH0109/Course/_2_foundations/sheet2D.lean
@@ -1,31 +1,23 @@
 import Mathlib.Tactic
 
 variable (A: Type) (P Q R: A → Prop)
+
 -- 01
 example : (∀x, ∃y, P y → P x) :=
 by
-  intros x
-  use x 
-  intro hx
-  exact hx
-
+  intro x
+  sorry
 
 -- 02
 example (h : ∃ x, ¬ P x) : ¬ ∀ a, P a:=
 by
-  intro hf
--- In the same way that `h : P ∧ Q` consists of two things, 
--- a proof of P and a proof of Q, so `∃x, P x` consists of two
--- things `x` and a proof of `P x`.  
-  obtain ⟨x,hx⟩:=h 
-  apply hx
-  apply hf
+  sorry
 
 
 /-
 We used the tactic `push_neg` to push negations inside a goal.
 
-We can also use this to simplify a negated hypothesis `h : ¬ P` 
+We can also use it to simplify a negated hypothesis `h : ¬ P` 
 with `push_neg at h`
 -/
 
@@ -39,30 +31,22 @@ by
 example : (∀ x, P x → Q x) → (∀y, Q y → R y) → (∀z, R z → P z) → (∀a, R a ↔ P a):=
 by
   intro hpq hqr hrp a
-  constructor
-  · apply hrp
-  · intro hpa
-    specialize hpq a
-    sorry
+  sorry
+  
 /-
-If our hypothesis is `h: ∃x, ∃y,...` then `cases h with x hx` will give us `x` and
- `hx : ∃y ,...` we can then do `cases hx with y hy`.
+If our hypothesis is `h: ∃x, ∃y,...` then `obtain ⟨x, y, hxy⟩ := h` will give us `x` and `y` 
 
 If our goal is `⊢ ∃y, ∃x, ...` we can first tell Lean what to use for `y` and then the 
-goal will change to `⊢ ∃ x,...`  so we can then tell it what to use for `x` -/
+goal will change to `⊢ ∃ x,...`  so we can then tell it what to use for `x`
+-/
 -- 05 
 example  (h : ∃x, ∃y, P x → P y) : (∃x, ∃y, P y → P x):=
 by
-  obtain ⟨x , hx⟩:=h
-  obtain ⟨y,hxy⟩:= hx
-  use y
-  use x 
-
+  sorry
 
 -- 06
 example : (∀ x, P x) → (∃x,¬ Q x) → ∃ (a b : A), ¬ (P a → Q b):=
 by
-  intros hp hq  
   sorry
 
 -- 07
@@ -83,8 +67,8 @@ by
 
 variable (C : A → A → Prop)
 -- 10 The no barber theorem 
+-- Hint : Use `by_cases` on something..
 example : ¬∃ b, ∀ a, C b a ↔ ¬ C a a :=
 by
   intro hb
   sorry
-
