dc1e92ffbfe30cc7a321b3c1207b2aeb1843f4dd
Richard <r.m.hill@ucl.ac.uk>
2023-10-04 17:52:51 +0100

    added (P : Prop) in a few cases

--- UCLMATH0109/Course/_2_foundations/C_not_false.lean
+++ UCLMATH0109/Course/_2_foundations/C_not_false.lean
@@ -8,9 +8,10 @@ We would like `not P` to be the proposition that is true iff P is False
 In particular if we have `P` and `¬P` in the local context then we have a `contradiction`
 and from this we can prove any proposition Q
 -/
-example (Q : Prop) (hp: P) (hnp: ¬ P) : Q :=
+example (P Q : Prop) (hp: P) (hnp: ¬ P) : Q :=
 by
   contradiction
+
 
 
 /-  To prove `¬ P` we need to show that assuming `P` we get a contradiction.
@@ -19,11 +20,12 @@ In Lean `¬ P` is formally defined as `P → False` so it is an implication.
 We can start a proof of `¬ P` with `intro hp` and then we try to derive a contradiction.
 -/
 
-example : ¬ (P ∧ ¬P):=
+example (P : Prop) : ¬ (P ∧ ¬P):=
 by
   intro h
   obtain ⟨hp,hnp⟩:= h
   contradiction
+  
 
 
 /-
@@ -32,18 +34,17 @@ Since **any** proposition `Q` follows from a contradiction we can always replace
 -/
 example (Q : Prop) (hf : False ) : Q :=
 by
--- Change the goal from `⊢ P` to `⊢ False`
+  -- Change the goal from `⊢ P` to `⊢ False`
   exfalso
 -- Complete the proof using `hf : False`  
   exact hf
 
 -- In our next example we could start with `intro hp`, `intro hn`
 -- but these two tactics can me merged into one.
-example : P → ¬¬P :=  
+example (P : Prop) : P → ¬¬P :=  
 by
-  intros hp hn
+  intro _ _
   contradiction
-
 
 /- 
 In order to prove that `P` and `¬¬P` are equivalent we need to assume the 
@@ -52,7 +53,7 @@ Law of the Excluded Middle, which says that for any proposition P we have:
 The Lean tactic `by_cases hp : P` allows us to use this to constructor a proof into 
 two goals where in the first we have `hp : P` and in the second we have `hp : ¬P`
 -/
-example : ¬¬ P → P:=
+example (P : Prop) : ¬¬ P → P:=
 by
   intro hnp
   by_cases hp : P
@@ -65,7 +66,7 @@ by
 # True : (tactic : triv)
 `True` is the proposition that is trivially true, its proof is `triv`
 -/
-example : P → True :=
+example (P : Prop) : P → True :=
 by
   intro -- we don't need to use a name for the `intro` since we won't use it
   triv
