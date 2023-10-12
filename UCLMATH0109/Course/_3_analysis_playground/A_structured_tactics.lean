6e12c7f5db12bcfbd253f1414b2bb63c1394ec46
jt496 <john.talbot@gmail.com>
2023-10-12 10:12:25 +0100

    prevideo 3A

--- UCLMATH0109/Course/_3_analysis_playground/A_structured_tactics.lean
+++ UCLMATH0109/Course/_3_analysis_playground/A_structured_tactics.lean
@@ -12,24 +12,24 @@ currently have in our local context by `?_` and Lean will add this as a new goal
 Many of the suggestions given by `apply?` will involve `refine`
 -/
 
+-- 01
 example (a b : ℝ) : |a - b| = |b - a| :=
 by
--- exact?
   sorry
 
+-- 02
 example (a b c : ℝ) : |a - b| ≤ |a - c| + |c - b| :=
 by
--- apply?
   sorry
 
+-- 03
 example (a b c : ℕ) (h : c < a)  : 0 < a + b - c := 
 by
-  --apply? --apply? 
   sorry
 
+-- 04
 example (n : ℕ) : n < (n + 1) + 1 :=
 by
-  --apply? --apply? 
   sorry
 
 
@@ -38,19 +38,19 @@ Congruence lemmas: if `f = g` and `a = b` then `f a = g b`. We can prove this ea
 but the tactic `congr!` will do it for us.
 -/
 
+-- 05
 example (f g : ℕ → ℝ) (a b : ℕ) (hac : a = b) (hfg : f = g): f a = g b :=
 by 
--- congr!
   sorry
 
 /-
 Sometimes `congr!` is too aggressive and results in goals that are false. We can control this using 
 `congr! n` where `n = 1,2,..`
-
 -/
+
+-- 06
 example (a b c : ℕ) : a + 2*b + 2*c = (2*b + a) + (c + c) :=
 by
---  congr! 1 -- apply? -- apply?
   sorry
 
 /-
@@ -61,19 +61,22 @@ by
  `convert h using n` where `n = 1, 2, ...`
 -/
 
+-- 07
 example (f g : ℕ → ℕ) (h : ∀ n, g n = f n) (hm : Monotone f) : Monotone g :=
 by
---  convert hm
---  apply?
   sorry
 
+-- 08
 example (f : ℕ → ℝ) (h : StrictMono (f + f)) : StrictMono (2 * f):=
 by
---  convert h using 1
---  apply?
   sorry
 
 /-
+We already saw the `rfl` tactic that allows Lean to close any goal of the form `⊢ A ∼ A`, if 
+`∼` is a reflexive relation. 
+
+There are corresponding tactics for symmetric and transitive relations. 
+
 If the goal is `⊢ A ∼ B` where `∼` is a symmetric relation then `symm` changes the goal to `⊢ B ∼ A`
 
 If `h : A ∼ B` is in the local context then `h.symm` is `B ∼ A`
@@ -81,28 +84,22 @@ If `h : A ∼ B` is in the local context then `h.symm` is `B ∼ A`
 The most common use of this is with `=`
 -/
 
+-- 09 
 example (a b : ℕ) (h : a = b) : b = a :=
 by
---  symm
---  exact h
   sorry
 
 /-
 If the goal is `⊢ A ∼ B` where `∼` is a transitive relation, then `trans C` converts this into two goals,
 `⊢ A ∼ C` and `⊢ C ∼ B` 
 -/  
-  
+
+-- 10  
 example (a b c : ℕ) (h1 : a = b) (h2 : c = b) : a = c :=
 by
---  trans b
---  exact h1
---  exact h2.symm
   sorry
 
-
+-- 11
 example (s t u : Set ℕ) (h1: s ⊆ t) (h2 : t ⊆ u) : s ⊆ u:=
 by
---  trans t
---  exact h1
---  exact h2
   sorry
