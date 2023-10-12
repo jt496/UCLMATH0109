b4bcaff97b70683bd85c20166fd8f30327feed67
jt496 <john.talbot@gmail.com>
2023-10-12 08:47:41 +0100

    renamed

--- UCLMATH0109/Course/_3_analysis_playground/A_useful_tactics.lean
+++ UCLMATH0109/Course/_3_analysis_playground/A_useful_tactics.lean
@@ -1,6 +1,8 @@
 import Mathlib.Tactic
 
 /-
+# Other useful tactics
+
 `refine` is like `exact` except that we can replace any explicit argument that we don't 
 currently have in our local context by `?_` and Lean will add this as a new goal. 
 
@@ -12,18 +14,22 @@ Many of the suggestions given by `apply?` will involve `refine`
 
 example (a b : ℝ) : |a - b| = |b - a| :=
 by
+-- exact?
   sorry
 
 example (a b c : ℝ) : |a - b| ≤ |a - c| + |c - b| :=
 by
+-- apply?
   sorry
 
 example (a b c : ℕ) (h : c < a)  : 0 < a + b - c := 
 by
+  --apply? --apply? 
   sorry
 
 example (n : ℕ) : n < (n + 1) + 1 :=
 by
+  --apply? --apply? 
   sorry
 
 
@@ -32,8 +38,9 @@ Congruence lemmas: if `f = g` and `a = b` then `f a = g b`. We can prove this ea
 but the tactic `congr!` will do it for us.
 -/
 
-example (f g : ℕ → ℕ) (a b : ℕ) (hac : a = b) (hfg : f = g): f a = g b :=
+example (f g : ℕ → ℝ) (a b : ℕ) (hac : a = b) (hfg : f = g): f a = g b :=
 by 
+-- congr!
   sorry
 
 /-
@@ -43,6 +50,7 @@ Sometimes `congr!` is too aggressive and results in goals that are false. We can
 -/
 example (a b c : ℕ) : a + 2*b + 2*c = (2*b + a) + (c + c) :=
 by
+--  congr! 1 -- apply? -- apply?
   sorry
 
 /-
@@ -53,12 +61,16 @@ by
  `convert h using n` where `n = 1, 2, ...`
 -/
 
-example (f g : ℕ → ℕ) (h : ∀ n, f n = g n) (hm : Monotone f) : Monotone g :=
+example (f g : ℕ → ℕ) (h : ∀ n, g n = f n) (hm : Monotone f) : Monotone g :=
 by
+--  convert hm
+--  apply?
   sorry
 
-example (f : ℕ → ℝ) (h : StrictMono (2 * f)) : StrictMono (f + f):=
+example (f : ℕ → ℝ) (h : StrictMono (f + f)) : StrictMono (2 * f):=
 by
+--  convert h using 1
+--  apply?
   sorry
 
 /-
@@ -71,17 +83,26 @@ The most common use of this is with `=`
 
 example (a b : ℕ) (h : a = b) : b = a :=
 by
+--  symm
+--  exact h
   sorry
 
-  
-example (a b c : ℕ) (h1 : a = b) (h2 : c = b) : a = c :=
-by
-  sorry
 /-
 If the goal is `⊢ A ∼ B` where `∼` is a transitive relation, then `trans C` converts this into two goals,
 `⊢ A ∼ C` and `⊢ C ∼ B` 
 -/  
+  
+example (a b c : ℕ) (h1 : a = b) (h2 : c = b) : a = c :=
+by
+--  trans b
+--  exact h1
+--  exact h2.symm
+  sorry
+
 
 example (s t u : Set ℕ) (h1: s ⊆ t) (h2 : t ⊆ u) : s ⊆ u:=
 by
+--  trans t
+--  exact h1
+--  exact h2
   sorry
