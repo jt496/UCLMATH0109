a229557559d75fee1ede89e4ddfd07e3009b9127
jt496 <john.talbot@gmail.com>
2023-10-12 12:23:21 +0100

    3B pre video

--- UCLMATH0109/Course/_3_analysis_playground/B_higher_tactics.lean
+++ UCLMATH0109/Course/_3_analysis_playground/B_higher_tactics.lean
@@ -25,7 +25,6 @@ by
 
 lemma sq_add' (a b : ℝ) : (a + b)^2 = a^2 + 2*a*b + b^2  :=
 by
-  --ring
   sorry
 
 #print sq_add
@@ -34,7 +33,6 @@ by
 -- You really wouldn't want to prove this by hand
 lemma pow_four_add (a b : ℝ) : (a + b)^4 = a^4 + 4*a^3*b+ 6*a^2*b^2 + 4*a*b^3 + b^4 :=
 by 
-  --ring
   sorry
 
 /-
@@ -44,17 +42,14 @@ Beware that in `ℕ` subtraction is truncated (so a - b = 0 if a ≤ b).
 
 lemma nat_add_sub (a b : ℕ) : a + b - b = a:=
 by
---  ring
---  exact?
   sorry
 
 /- 
 If we have a goal that is involves numerical expressions then `norm_num` may be able to close it.
 -/
 
-lemma less_than : (123123123123123 : ℝ)< 212312312312312 :=
+lemma less_than : (123123123123123 : ℝ) < 212312312312312 :=
 by
---  norm_num
   sorry
 
 /-
@@ -63,14 +58,11 @@ We can also use `decide` for proving propositions
 
 lemma small_prime : Nat.Prime 13 :=
 by
---  decide
   sorry
 
 -- However sometimes the algorithm called by `decide` will time-out
 lemma prime: Nat.Prime 110017 :=
 by
-  --decide fails
-  -- norm_num
   sorry
 
 /-
@@ -79,7 +71,6 @@ by
 
 lemma linear_ineq (a b c : ℝ) (h1: a ≤ 2 * b) (h2: b ≤ 3 * c) : 2 * a ≤ 12 *c:=
 by
---  linarith
   sorry
 
 /-
@@ -88,8 +79,6 @@ For non-linear inequalities we can try `nlinarith`
 
 example (a b : ℝ) : 0 ≤ (a + b)^2 - 2*a*b :=
 by
-  --ring
-  --nlinarith
   sorry
 
 
