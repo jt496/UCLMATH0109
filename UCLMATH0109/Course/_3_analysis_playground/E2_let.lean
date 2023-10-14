720131893e6de1c66cea9554f1a035e2574f71ba
jt496 <john.talbot@gmail.com>
2023-10-14 13:44:09 +0100

    E2_let post video

--- UCLMATH0109/Course/_3_analysis_playground/E2_let.lean
+++ UCLMATH0109/Course/_3_analysis_playground/E2_let.lean
@@ -35,31 +35,27 @@ To prove that `C` works we need to `intro n` and split `by_cases hn : n ≤ K`
 #check lt_add_of_sub_left_lt
 #check abs_sub_abs_le_abs_sub 
 
-
-
-
-
-
 theorem sLim_imp_bd (hx : limₙ x a) : ∃ C, ∀ n, |x n| ≤ C :=
 by
 -- Get K : ℕ from definition of limₙ x a with ε = 1
-  specialize hx 1
+  specialize hx 1 (by norm_num)
+  obtain ⟨K,hK⟩ := hx
 -- Let I = {0,1,...,K}
-
+  let I := range K.succ
 -- I is Nonempty
-
+  have hne : I.Nonempty := by exact nonempty_range_succ
 -- J = {|x 0|, |x 1|,... ,|x K|} the image of I under |x|
-
+  let J := I.image |x| 
 -- Let B = max' J (exists since J is a Nonempty Finset ℕ)
-
+  let B := J.max' (by exact Nonempty.image hne |x|)
 -- We use the bound C = max B (|a| + 1) (note this is the max of a pair of Nats)
-
+  use max B (|a| + 1)
 -- |x n| is always ≤ either B1 or (|a| + 1) depending on n
-
+  intro n
 -- Do a by_cases split on `n ≤ K` 
- 
- -- In this case |x n| ≤ B1 because |x n| ∈ J 
- 
+  by_cases hn : n ≤ K 
+ -- In this case |x n| ≤ B because |x n| ∈ J 
+  · sorry 
  -- Now need to prove that n ∈ I which is true since n ≤ K 
-  sorry
+  · sorry
     
