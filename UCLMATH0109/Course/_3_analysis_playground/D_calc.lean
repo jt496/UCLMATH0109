69255885ba1d9552a19dbf14128bab9f4c561a04
Richard <r.m.hill@ucl.ac.uk>
2023-10-15 13:01:51 +0100

    changes to D_calc

--- UCLMATH0109/Course/_3_analysis_playground/D_calc.lean
+++ UCLMATH0109/Course/_3_analysis_playground/D_calc.lean
@@ -1,5 +1,3 @@
--- import Mathlib.Tactic.Basic
--- import Mathlib.Data.Real.Basic
 import UCLMATH0109.Course._3_analysis_playground.C_have
 
 
@@ -16,7 +14,7 @@ example (x y a b : ℚ) (hx : x = a^2 - b^2) (hy : y = 2 * a * b) (hab : a^2 + b
   x^2 + y^2 = 1 :=
 by
   calc
-  x^2 + y^2 = (a^2-b^2)^2 + (2*a*b)^2                     := by rw [hx, hy]
+  x^2 + y^2 = (a^2-b^2)^2 + (2*a*b)^2                     := by rw [hx,hy]
   _         = a^4 - 2 * a^2 * b^2 + b^4 + 4 * a^2 * b^2   := by ring
   _         = a^4 + 2 * a^2 * b^2 + b^4                   := by ring
   _         = (a^2 + b^2)^2                               := by ring
@@ -29,15 +27,15 @@ When dealing with inequalities in `calc`-blocks, the tactic `rel` is often usefu
 `rel` is similar to `rw`, but substitutes inequalities rather than equalities.
 -/
 
-example (a b x y : ℝ) (hx : x = a+2*b) (hy : |a-b| ≤ y) (ha : 10 ≤ a) :
-  30 ≤ x + 2 * y :=
+example (a b x y : ℝ) (hx : x = a+2*b) (hy : |a-b| ≤ y) (ha : 10 < a) :
+  x + 2 * y > 30  :=
 by
   calc
   _ = a + 2*b + 2*y       := by rw [hx]
   _ ≥ a + 2*b + 2*|a-b|   := by rel [hy]
   _ ≥ a + 2*b + 2*(a-b)   := by rel [le_abs_self (a-b)]
   _ = 3 * a               := by ring
-  _ ≥ 3 * 10              := by rel [ha]
+  _ > 3 * 10              := by rel [ha]
   _ = 30                  := by norm_num
 
 
@@ -48,28 +46,24 @@ Here is a simple example, proving that limits of sequences are unique.
 -/
 theorem sLim_unique (ha : limₙ x a) (hb : limₙ x b) : a = b :=
 by
-  --- If a ≠ b then we can set ε = |a-b| > 0 and obtain a contradiction
-  have : 0 < |a-b| ∨ |a-b| = 0
-  · sorry --apply? ; exact?
-  cases this with
-  | inl hp =>
-    exfalso
-    rw [sLim] at ha hb
-    have : 0 < |a-b|/2
-    · exact?
-    specialize ha _ this
-    specialize hb _ this
-    obtain ⟨Na, hA⟩ := ha
-    obtain ⟨Nb, hB⟩ := hb
-    let N := max Na Nb
-    specialize hA N (by exact?)
-    specialize hB N (by exact?)
-    have := calc
-       |a - b| = |(a - x N) + (x N - b)|   := by ring
-        _      ≤ |a - x N| + |x N - b|     := by exact?
-        _      = |x N - a| + |x N - b|     := by congr 1; exact?
-        _      < |a - b|/2 + |a - b|/2     := by rel [hA,hB]
-        _      = |a - b|                   := by ring
-    exact?
-  | inr hz =>  
-    exact eq_of_abs_sub_eq_zero hz
+  by_contra h
+  rw [sLim] at ha hb
+  let ε := |a - b| / 2
+  have : ε > 0
+  · refine half_pos ?this.h
+    refine abs_pos.mpr ?this.h.a
+    exact sub_ne_zero.mpr h
+  specialize ha ε this
+  specialize hb ε this
+  obtain ⟨Na, hA⟩ := ha
+  obtain ⟨Nb, hB⟩ := hb
+  let N := max Na Nb
+  specialize hA N (by exact Nat.le_max_left Na Nb)
+  specialize hB N (by exact Nat.le_max_right Na Nb)
+  have := calc
+    |a - b| = |(a - x N) + (x N - b)|   := by ring
+     _      ≤ |a - x N| + |x N - b|     := by exact abs_add (a - x N) (x N - b)
+     _      = |x N - a| + |x N - b|     := by congr 1; exact abs_sub_comm a (x N)
+     _      < ε + ε                     := by rel [hA,hB]
+     _      = |a - b|                   := by ring
+  exact LT.lt.false this
