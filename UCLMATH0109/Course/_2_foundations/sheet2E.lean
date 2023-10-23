a2e8c5304946addd0d568bdc1448146a51112b4b
jt496 <john.talbot@gmail.com>
2023-10-23 22:00:17 +0100

    sheet 2e more

--- UCLMATH0109/Course/_2_foundations/sheet2E.lean
+++ UCLMATH0109/Course/_2_foundations/sheet2E.lean
@@ -1,50 +1,59 @@
 import Mathlib.Tactic
-
+import Mathlib.Data.Nat.Basic
 
 /-
-# TODO write some simple questions using rfl, rw and rwa (but not using theorems from Mathlib)
+  All of these examples can be solved using only `rw`, `rwa` and `rfl`
 -/
 
 -- 01
-example (a b : ℕ) (ha : a = 1) (hb: b = 2) : a+ 2*b +3 = 8 :=
+example (ha : a = 1) (hb: b = 2*a) : a + 2 * b + 3 = 8 * a :=
 by
-  rw [ha,hb]
+-- When Lean sees a numeral it assumes this is of type ℕ (unless told otherwise),
+-- so from the hypotheses it can deduce that a and b are also of type ℕ
+  rw [hb, ha]
 
--- 02 
-example (P Q : Prop) (hp: P ↔ Q) (hq : Q): P:=
+-- 02
+example (h1: a = b) (h2 : c = d) (h3: d = b) : a = c:=
+by
+-- Notice that Lean can work out that a,b,c and d are all of the same type
+-- even though we haven't told it what type this is.
+  rw [h1,h2,h3]
+
+-- 03
+example (hp: P ↔ Q) (hq : Q): P:=
 by
   rwa [hp]
 
--- 03
-example (α : Type) (P Q: α → Prop) (hP: ∀ x, P x ↔ Q x) (a : α) : P a ↔ Q a:=
+-- 04
+example (α : Type) (P Q R: α → Prop) (hP: ∀ x, P x ↔ Q x) (a : α) (hQR: Q = R): P a ↔ R a:=
 by
-  rw [hP]
+  rw [hP, hQR]
 
 
 /-
 In our next example we have a type `α` for which multiplication on the left by ℕ is defined
-(so if `n : ℕ` and `a : α` then `n * a : α` is well-defined.) 
+(so if `n : ℕ` and `a : α` then `n * a : α` is well-defined.)
 -/
 variable {α : Type} [HMul ℕ α α]
 /-
-This multiplication satisfies two hypotheses: 
-`h2id: ∀ a, a = 2 * a` so 2 is a left-multiplicative identity 
+This multiplication satisfies two hypotheses:
+`h2id: ∀ a, a = 2 * a`, so 2 is a left-multiplicative identity
 `hmul_assoc : ∀ i j : ℕ, ∀ a : α, i * (j * a) = (i * j ) * a`, so
 left-multiplication by natural numbers is associative.
 -/
 -- 08
-example (h2id: ∀ (a : α), a = 2 * a ) (hmul_assoc : ∀ i j: ℕ,∀ a : α, i * (j * a) = (i * j ) * a) (b : α): 
+example (h2id: ∀ (a : α), a = 2 * a ) (hmul_assoc : ∀ i j: ℕ,∀ a : α, i * (j * a) = (i * j ) * a) (b : α):
  b = 8 * b:=
 by
   rw [h2id b, hmul_assoc, h2id (2*b), h2id (2*b), h2id (2*b), hmul_assoc,hmul_assoc,hmul_assoc]
 
 -- 09 Bonus question using induction
-example (h2id: ∀ (a : α), a = 2 * a ) (hmul_assoc : ∀ i j: ℕ,∀ a : α, i * (j * a) = (i * j ) * a) (b : α) (n : ℕ): 
+example (h2id: ∀ (a : α), a = 2 * a ) (hmul_assoc : ∀ i j: ℕ,∀ a : α, i * (j * a) = (i * j ) * a) (b : α) (n : ℕ):
  b = 2^n * b:=
 by
   rw [h2id b,hmul_assoc]
   induction n with
-  | zero => rfl; 
-  | succ n ih => 
-    rw [ih,← hmul_assoc,h2id (2*b),hmul_assoc,hmul_assoc]
+  | zero => rfl;
+  | succ n ih =>
+    rw [ih, ← hmul_assoc, h2id (2*b), hmul_assoc, hmul_assoc]
     rfl
