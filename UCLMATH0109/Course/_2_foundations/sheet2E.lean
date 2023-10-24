0e6fd0aa942bd9bff6efbf6db74f845aca9a0b17
jt496 <john.talbot@gmail.com>
2023-10-24 09:30:42 +0100

    sheet 2e done

--- UCLMATH0109/Course/_2_foundations/sheet2E.lean
+++ UCLMATH0109/Course/_2_foundations/sheet2E.lean
@@ -6,69 +6,84 @@ import Mathlib.Data.Nat.Basic
 -/
 
 -- 01
-example (ha : a = 1) (hb: b = 2*a) : a + 2 * b + 3 = 8 * a :=
+example (ha : a = 1) (hb : b = 2*a) : a + 2 * b + 3 = 8 * a :=
 by
 -- When Lean sees a numeral it assumes this is of type ℕ (unless told otherwise),
--- so from the hypotheses it can deduce that a and b are also of type ℕ
+-- so from the hypotheses Lean deduces that a and b are also of type ℕ (see Infoview)
   rw [hb, ha]
 
 -- 02
-example (h1: a = b) (h2 : c = d) (h3: d = b) : a = c:=
+example (h1 : a = b) (h2 : c = d) (h3 : d = b) : a = c :=
 by
--- Notice that Lean can work out that a,b,c and d are all of the same type
+-- Notice that Lean can work out that a,b,c and d must all have same type (how?)
 -- even though we haven't told it what type this is.
   rw [h1,h2,h3]
 
 -- 03
-example (hp: P ↔ Q) (hq : Q): P:=
+example (hp : P ↔ Q) (hq : Q): P:=
 by
   rwa [hp]
 
 -- 04
-example (α : Type) (P Q R: α → Prop) (hP: ∀ x, P x ↔ Q x) (a : α) (hQR: Q = R): P a ↔ R a:=
+example (α : Type) (P Q R : α → Prop) (hP : ∀ x, P x ↔ Q x) (a : α) (hQR : Q = R): P a ↔ R a:=
 by
   rw [hP, hQR]
 
+-- 05
+example  (h2 : R ↔ Q) (h3 : R ↔ P) (hp : P) : Q :=
+by
+  rw [← h3] at hp
+  rwa [← h2]
+
 
 /-
-In our next few examples we have a type `α` for which multiplication on the left by ℕ is defined
-(so if `n : ℕ` and `a : α` then `n * a : α` is well-defined.)
+In our last few examples we introduce a type `α` for which multiplication on the left by ℕ is defined
+(so if `n : ℕ` and `a : α` then `n * a` is well-defined and has type `α`.)
 -/
 variable {α : Type} [HMul ℕ α α]
+
 -- This multiplication satisfies two axioms:
 
--- Left-multiplication by natural numbers is associative.
+-- Left-multiplication by ℕ is associative.
 (hmul_assoc : ∀ i j : ℕ, ∀ a : α, i * (j * a) = (i * j ) * a)
--- 2 is a left-identity
+
+-- Two is a left-identity
 (h2id: ∀ a : α, a = 2 * a)
 
 -- Note that we know **nothing** else about this multiplication, even `b = 1 * b` needs a proof.
 
+-- However Lean does know how to do arithmetic in ℕ so, for example, if `a : α` then
+--  `(2 * 3) * a = 6 * a` is true by `rfl`
+
+-- 06
+example (b : α) : 12 * b = 3 * 4 * b :=
+by
+  rfl
+
+-- 07 associativity does not hold by definition so rfl won't work here.
+example (b : α) : 10 * b = 5 * (2 * b) :=
+by
+--  rfl fails
+  rw [hmul_assoc]
+
 -- 08
-example (b : α) :
- b = 1 * b :=
+example (b : α) : b = 1 * b :=
 by
   rw [h2id b, hmul_assoc]
 
 -- 09
-example (b : α):
- b = 8 * b:=
+example (b : α) : b = 8 * b:=
 by
   rw [h2id b, hmul_assoc, h2id (2*b), h2id (2*b), h2id (2*b), hmul_assoc,hmul_assoc,hmul_assoc]
 
 -- 10
-example (h34: ∀ a : α, 3 * a = 4 * a) (b : α): b = 3 * b:=
+example (b : α) : 3 * b = 12 * b :=
 by
-  rw [h34, h2id b, hmul_assoc, h2id (2*b), h2id (2*b), hmul_assoc, hmul_assoc]
+  rw [h2id b, hmul_assoc 12, h2id (2*b), h2id (2*b), hmul_assoc, hmul_assoc, hmul_assoc]
 
--- 11
-example (ho: ∀ n : ℕ, ∀ a : α, (2*n)* a = (2*n + 1)* a) (k : ℕ): a = k*a :=
-by
 
-  sorry
-
--- 12 Bonus question using induction
-example (h2id: ∀ (a : α), a = 2 * a) (hmul_assoc : ∀ i j : ℕ, ∀ a : α, i * (j * a) = (i * j ) * a) (b : α) (n m: ℕ):
+-- 11 Bonus question using induction for those who know it
+example (b : α) (n : ℕ):
  b = 2^n * b:=
 by
   rw [h2id b,hmul_assoc]
