5175f9f5ffb6757b40f2ecdcb7b13266c364167c
jt496 <john.talbot@gmail.com>
2023-10-25 15:58:52 +0100

    started sheet 3A

--- UCLMATH0109/Course/_3_analysis_playground/sheet3A.lean
+++ UCLMATH0109/Course/_3_analysis_playground/sheet3A.lean
@@ -10,14 +10,20 @@ TODO: Add more examples of using refine / congr! / convert etc
 def sLim (x : ℕ → ℝ) (a : ℝ) : Prop :=
   ∀ ε, 0 < ε → ∃ N, ∀ n, N ≤ n → |x n - a| < ε
 
+notation "limₙ " => sLim
 
-notation "limₙ " => sLim 
-
--- 01 
-example (x y: ℕ → ℝ) (hxa: limₙ x a) (ha: b = a) (hxy: ∀ n, y n = x n) : limₙ y b :=
+-- 01
+lemma sLim_congr  (hxa: limₙ x a)  (hxy: ∀ n, y n = x n) (ha: b = a) : limₙ y b :=
 by
   -- you can do this with `rw`, but `convert` is more direct
   convert hxa
   sorry
 
-
+-- 02 If `xₙ → a` then `xₙ₊ₘ → a` for all m
+lemma tail_sLim_of_sLim (m : ℕ) (hx : limₙ x a) : limₙ (fun n => x (n + m)) a :=
+by
+  intro ε hε; dsimp
+  -- ⊢ ∃ N, ∀ (n : ℕ), N ≤ n → |x (n + m) - a| < ε
+  -- You now need to use `hx : limₙ x a` to get a suitable `N`
+  -- To finish the proof `apply?` may be useful.
+  sorry
