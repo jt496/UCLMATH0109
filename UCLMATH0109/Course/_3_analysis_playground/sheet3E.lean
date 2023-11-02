08c98d801dba88d1b0b9ba730b8f70312d2b215f
Richard <r.m.hill@ucl.ac.uk>
2023-11-02 14:35:18 +0000

    finset example

--- UCLMATH0109/Course/_3_analysis_playground/sheet3E.lean
+++ UCLMATH0109/Course/_3_analysis_playground/sheet3E.lean
@@ -4,11 +4,19 @@ import Mathlib.Tactic
 
 open Finset
 open scoped BigOperators
+noncomputable section
 
-/-
-TODO write sheet on Finset examples
--/
+def c : ℕ → ℝ := fun n ↦ 1 / ((n+1)*(n+2))
+
+def S : ℕ → ℝ := fun N ↦ ∑ n in range N, c n
+
+lemma c_def : c n = 1 / ((n+1)*(n+2)) := rfl
+
+lemma S_def : S N = ∑ n in range N, c n := rfl
 
 
-
-
+theorem S_eq : S N = 1 - 1 / (N+1) :=
+by
+  have partial_frac : ∀ n, c n = 1/(n+1 : ℝ) - 1/(n+2 : ℝ)
+  · sorry
+  sorry
