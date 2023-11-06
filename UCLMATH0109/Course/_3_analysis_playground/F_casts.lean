89dc541ec7ee24b40262479c1135111a47de2de8
jt496 <john.talbot@gmail.com>
2023-11-06 20:48:46 +0000

    post video F_cast

--- UCLMATH0109/Course/_3_analysis_playground/F_casts.lean
+++ UCLMATH0109/Course/_3_analysis_playground/F_casts.lean
@@ -6,7 +6,7 @@ sense in Lean.
 -/
 section
 variable (α β : Type) (a : α) (b : β)
--- #check a = b
+--#check a = b
 /-
 type mismatch
   b
@@ -25,7 +25,7 @@ of different types.
 For example if `n : ℕ` and `x : ℝ` then `x = n` is a perfectly reasonable proposition.
 
 Lean requires us to convert the smaller type ℕ to the larger type ℝ.
-This is called a `coercion` or `cast`
+This is called a `cast` or `coercion`
 -/
 variable (n : ℕ) (x : ℝ)
 #check x = n -- x = ↑n, th `↑` in the Infoview indicates that a coercion has taken place
@@ -41,27 +41,30 @@ See https://lean-forward.github.io/norm_cast/norm_cast.pdf for more details.
 -- 01
 example (a b : ℕ) : a + b = (a : ℤ) + b :=
 by
-  sorry
+  rfl
 
 -- 02
 example (a b : ℕ) : (a + b : ℕ) = (a : ℝ) + b :=
 by
-  sorry
+  push_cast
+  rfl
 
 -- 03
 example (n : ℕ) : (2 * n : ℝ) + 3  = (2 * n : ℤ) + 3 :=
 by
-  sorry
+  push_cast
+  rfl
 
 -- 04
 example (a b : ℕ) (c : ℤ) : (a + b : ℕ) + (c : ℤ) = (((a : ℤ) + (b + c : ℚ) : ℝ) : ℂ) :=
 by
-  sorry
+  push_cast
+  apply add_assoc
 
 -- 05
 example (n : ℕ) (z : ℤ) (h : n - z < (5 : ℚ)) : n - z < 5 :=
 by
-  sorry
+  norm_cast at h
 
 
 -- If (a b : ℕ) and a ≤ b then a - b = 0 (subtraction is `truncated` in ℕ)
@@ -73,19 +76,21 @@ by
 -- 07
 example (a b : ℕ) : (a : ℤ) - (a + b) = -b :=
 by
-  sorry
+  exact sub_add_cancel' _ _
 
 
 -- 08
 example (a b c: ℕ) (h : c = a + b) : (a : ℤ) - c = -b :=
 by
-  sorry
+  rw [h]
+  push_cast
+  exact sub_add_cancel' _ _
 
 
 -- 09
 example (a b : ℕ) (h : a ≤ b) : (b - a : ℕ)  = (b : ℤ)  - a:=
 by
-  sorry
+  norm_cast
 
 
 /-
@@ -96,28 +101,38 @@ the remainder of n mod d,
 -- 10
 example (n d : ℕ) (h: d ∣ n) : (n / d) * d = n :=
 by
-  sorry
+  exact Nat.div_mul_cancel h
 
 -- 11
 example (a b c : ℕ) (hb: b ∣ a) (hc: c ∣ a) (ha : a ≠ 0)   :
 ((a / b : ℕ)) / (a / c : ℕ) = (c : ℝ )/ b :=
 by
   have hab: ((a / b : ℕ) : ℝ) = (a : ℝ) / b
-  · sorry
+  · norm_cast
   have hac: ((a / c : ℕ) : ℝ) = (a : ℝ) / c
-  · sorry
-  sorry
+  · norm_cast
+  rw [hab, hac]
+  ring
+  rw [inv_inv]
+  congr
+  rw [mul_comm,←mul_assoc,inv_mul_cancel,one_mul]
+  norm_cast
 
 
 -- 12
 example (a b : ℕ) (h: a ∣ b) : (a : ℤ) ∣ (-(b : ℤ)):=
 by
-  sorry
+  obtain ⟨c,rfl⟩ := h
+  push_cast
+  use (-c)
+  exact Int.neg_mul_eq_mul_neg ↑a ↑c
 
 -- 13
 example (a b : ℕ) (z : ℤ) (ha : z ≤ a) (hb : z ≤ b) : z ≤ min (a : ℝ) b :=
 by
-  sorry
+  norm_cast
+  push_cast
+  apply le_min ha hb
 
 /-
 A useful tactic for cancelling denominators is `cancel_denoms`
@@ -128,4 +143,11 @@ open Finset
 -- 14
 example (n : ℕ) : ∑ i in range n.succ, (i : ℝ)^(3 : ℕ) = (n : ℝ)^2 * (n + 1 : ℝ)^2/4 :=
 by
-  sorry
+  cancel_denoms
+  induction n with
+  | zero =>
+    norm_cast
+  | succ n ih =>
+    rw [sum_range_succ, mul_add, ih, Nat.succ_eq_add_one]
+    push_cast; ring
+    
