54355f993bb9a418639b14944dc1ec6bc84c445b
jt496 <john.talbot@gmail.com>
2023-11-07 13:57:55 +0000

    sheet 3F done

--- UCLMATH0109/Course/_3_analysis_playground/sheet3F.lean
+++ UCLMATH0109/Course/_3_analysis_playground/sheet3F.lean
@@ -1,18 +1,61 @@
 import Mathlib.Tactic
 
 
+/-
+These examples should be solved mainly using `norm_cast`, `push_cast` and (occaisionally) `cancel_denoms`.
 
-#check Nat.div_mul_div_comm
-#check Nat.mul_div_mul
+Other useful tactics include: `ring`, `linarith`, `norm_num` and `apply?`.
 
+You will also need to find rewrite lemmas (and introduce `have` statements where needed).
+-/
+
+
+-- 01
+example (a : ℕ)  (b : ℚ) (h : (a : ℝ) = b): (a : ℂ) = b  :=
+by
+/-
+a : ℕ
+b : ℚ
+h : ↑a = ↑b
+⊢ ↑a = ↑b
+Note that `exact h` fails here even though Infoview suggests otherwise (hover over Infoview to see why)  -/
+  sorry
+
+
+-- 02
+example (a b c : ℕ) (h : a*b = 3*c) : (2*c : ℝ) = (2*a*b)/3:=
+by
+  sorry
+
+
+-- 03
+example (a b c : ℕ) (h : a + b + c = 3*c) : (2*c : ℝ) = a + b:=
+by
+  sorry
+
+
+/- Remember you can do `norm_cast at h` if `h` is in the local context -/
+-- 04
+example (a b c d : ℕ) (h1 : a + b = (c + (d : ℤ) : ℝ)) (h2: a ≤ (c : ℝ)) (h3 : b ≤ (d : ℤ)) : a = c :=
+by
+  sorry
+
+
+-- 05
+example (a b  : ℕ) (c : ℤ) (h1: a ∣ 2*b)  (h2 : (4*b : ℤ) ∣ -c) : (a : ℤ) ∣ c :=
+by
+  sorry
 /-
 If `d n : ℕ` and we have the hypothesis `h: d ∣ n` then norm_cast can prove
 that `((n / d : ℕ) : ℝ)` equals `(n : ℝ)/(d : ℝ)`, but without this hypothesis
 it simply isn't true.
 
 In the next example you may want to prove an extra divisibility hypothesis before
-using norm_cast.
--/
+using norm_cast. The following two results may also be useful. -/
+#check Nat.div_mul_div_comm
+#check Nat.mul_div_mul
+
+-- 06
 example (a b c : ℕ) (h : b ≠ 0) (h1 : b ∣ a) (h2 : c ∣ b):
 (a / b : ℕ) * (b / c : ℕ) = (a : ℝ) / c :=
 by
@@ -44,7 +87,7 @@ with `norm_cast` and `apply?`
 #check Nat.choose_succ_succ -- Pascal's identity
 
 /- If k ≤ n then `(n choose k + 1)/(n + 1 choose k + 1) < 1 -/
-
+-- 07
 example (n : ℕ) (h : k ≤ n): (n.choose (k + 1) : ℝ) / ((n + 1).choose (k + 1)) < 1:=
 by
   sorry
