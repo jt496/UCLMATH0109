77476bac3f9674f5de0be03555b93415ffaf109d
jt496 <john.talbot@gmail.com>
2024-01-07 20:50:17 +0000

    comment

--- UCLMATH0109/Course/_2_foundations/C_not_false.lean
+++ UCLMATH0109/Course/_2_foundations/C_not_false.lean
@@ -16,7 +16,7 @@ by
 
 /-  To prove `¬ P` we need to show that assuming `P` we get a contradiction.
 
-In Lean `¬ P` is formally defined as `P → False` so it is an implication. 
+In Lean `¬ P` is formally defined as `P → False` so it is an implication.
 We can start a proof of `¬ P` with `intro hp` and then we try to derive a contradiction.
 -/
 
@@ -25,7 +25,7 @@ by
   intro h
   obtain ⟨hp,hnp⟩:= h
   contradiction
-  
+
 
 
 /-
@@ -36,21 +36,21 @@ example (Q : Prop) (hf : False ) : Q :=
 by
   -- Change the goal from `⊢ P` to `⊢ False`
   exfalso
--- Complete the proof using `hf : False`  
+-- Complete the proof using `hf : False`
   exact hf
 
 -- In our next example we could start with `intro hp`, `intro hn`
 -- but these two tactics can me merged into one.
-example (P : Prop) : P → ¬¬P :=  
+example (P : Prop) : P → ¬¬P :=
 by
   intro _ _
   contradiction
 
-/- 
-In order to prove that `P` and `¬¬P` are equivalent we need to assume the 
+/-
+In order to prove that `P` and `¬¬P` are equivalent we need to assume the
 Law of the Excluded Middle, which says that for any proposition P we have:
  `P ∨ ¬ P`
-The Lean tactic `by_cases hp : P` allows us to use this to constructor a proof into 
+The Lean tactic `by_cases hp : P` allows us to use this to construct a proof into
 two goals where in the first we have `hp : P` and in the second we have `hp : ¬P`
 -/
 example (P : Prop) : ¬¬ P → P:=
