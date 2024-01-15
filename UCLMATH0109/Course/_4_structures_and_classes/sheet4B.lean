5d3fb15d65bd768633ad471d9ca99ea433b3aa1f
jt496 <john.talbot@gmail.com>
2024-01-15 14:58:08 +0000

    sheet 4B updated

--- UCLMATH0109/Course/_4_structures_and_classes/sheet4B.lean
+++ UCLMATH0109/Course/_4_structures_and_classes/sheet4B.lean
@@ -16,7 +16,7 @@ namespace VectorSpace
 
 section Simple_lemmas
 
-variable (V : Type) [VectorSpace V] (u v w : V)
+variable {V : Type} [VectorSpace V] (u v w : V)
 /-
 Prove a few simple statements about abstract vector spaces.
 -/
@@ -27,7 +27,7 @@ by
   sorry
 
 -- #02
-lemma zero_add : 0 + v = 0 :=
+lemma zero_add : 0 + v = v :=
 by
   sorry
 
@@ -36,12 +36,20 @@ lemma eq_neg_of_add_eq_zero (h : v + w = 0) : w = -v :=
 by
   sorry
 
+
 -- #04
+lemma add_left_cancel (h : v + w = w) : v = 0 :=
+by
+  have h2: v = v + w + - w
+  · sorry
+  sorry
+
+-- #05
 lemma zero_smul : (0:ℝ) • v = 0 :=
 by
   sorry
 
--- #05
+-- #06
 lemma neg_one_smul : (-1:ℝ) • v = -v :=
 by
   sorry
@@ -68,19 +76,27 @@ lemma map_smul (T : LinearMap V W) (x : ℝ) (v : V) : T (x • v) = x • T v :
   LinearMap.map_smul _ _ _
 
 
---#06
-lemma map_zero (T : LinearMap V W) : T 0 = 0 :=
-  sorry
-
 --#07
-lemma map_neg (T : LinearMap V W) (v : V) : T (-v) = -T v :=
+lemma map_zero (T : LinearMap V W) : T 0 = 0 :=
+by
   sorry
 
 --#08
+lemma map_neg (T : LinearMap V W) (v : V) : T (-v) = -T v :=
+by
+  sorry
+
+--#09
 def comp (T₁ : LinearMap V W) (T₂ : LinearMap U V) : LinearMap U W where
   toFun := T₁ ∘ T₂
-  map_add := sorry
-  map_smul := sorry
+  map_add :=
+  by
+    intro v w
+    dsimp -- without the obvious `rw` won't work
+    sorry
+  map_smul :=
+  by
+    sorry
 
 end Linear_maps
 
@@ -91,9 +107,10 @@ Now let's create an instance of a `VectorSpace`
 -/
 
 /-
-Make `SpaceTime` into a vector space ove the real numbers,
+Make `SpaceTime` into a vector space over the real numbers,
 with appropriate definitions of addition, scalar multiplication etc.
 -/
+@[ext]
 structure SpaceTime where
   x : ℝ
   y : ℝ
@@ -102,29 +119,29 @@ structure SpaceTime where
 
 namespace SpaceTime
 
---#09 Fill in the definitions of addition, negation and scalar multiplication.
+--#10 Fill in the definitions of addition, negation and scalar multiplication.
 def zero : SpaceTime := ⟨0,0,0,0⟩
 def add (P Q: SpaceTime) : SpaceTime := sorry
 def neg (P : SpaceTime) : SpaceTime := sorry
-def smul (x : ℝ) (P : SpaceTime) : SpaceTime := sorry
+def smul (a : ℝ) (P : SpaceTime) : SpaceTime := sorry
 
---#10 Create instances of the classes `Zero`, `Add`, `Neg` and `SMul`.
+--#11 Create instances of the classes `Zero`, `Add`, `Neg` and `SMul`.
 instance : Zero SpaceTime := sorry
 instance : Add SpaceTime := sorry
 instance : Neg SpaceTime := sorry
 instance : SMul ℝ SpaceTime := sorry
 
 
---#11 State and prove some definitional lemmas.
+--#12 State and prove some definitional lemmas.
 lemma zero_def : (0 : SpaceTime) = ⟨0,0,0,0⟩ := sorry
 lemma add_def (P Q : SpaceTime) : P + Q = sorry := sorry
 lemma neg_def (P : SpaceTime) : -P = sorry := sorry
-lemma smul_def (x : ℝ) (P : SpaceTime) : x • P = sorry := sorry
+lemma smul_def (a : ℝ) (P : SpaceTime) : a • P = sorry := sorry
 
 
 
---#12 Show that `SpaceTime` is a vector space over the real numbers.
-
+--#13 Show that `SpaceTime` is a vector space over the real numbers.
+-- Use the definitional lemmas we have just proved, along with `ext`.
 instance : VectorSpace SpaceTime := sorry
 
 end SpaceTime
