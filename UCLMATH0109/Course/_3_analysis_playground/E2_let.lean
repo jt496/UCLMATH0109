1dd909ef3e85f365ba62d021d0b2de55d7884172
jt496 <john.talbot@gmail.com>
2024-01-29 10:46:40 +0000

    e2 comment fixed

--- UCLMATH0109/Course/_3_analysis_playground/E2_let.lean
+++ UCLMATH0109/Course/_3_analysis_playground/E2_let.lean
@@ -13,8 +13,8 @@ We will prove this using Finsets.
 
 Recall `xₙ → a` is `∀ ε, ε > 0 → ∃ K, ∀ n, K ≤ n → |x n - a | < ε`
 
-An obvious way to prove the result is to set `ε = 1` and so obtain `K : ℕ` 
-such that `∀ n, K ≤ n → |x n - a| < 1`, 
+An obvious way to prove the result is to set `ε = 1` and so obtain `K : ℕ`
+such that `∀ n, K ≤ n → |x n - a| < 1`,
 
 Let `B  = max' {|x 0|,...,|x K|}`  then we can set `C = max B (|a| + 1)`
 
@@ -25,15 +25,15 @@ To prove that `C` works we need to `intro n` and split `by_cases hn : n ≤ K`
 -- The following results may be useful to complete the proof
 
 -- In the case `hn : n ≤ K`
-#check le_max_of_le_left   
+#check le_max_of_le_left
 #check le_max'
 #check mem_image_of_mem
 #check mem_range_succ_iff
 
--- In the case `hn : ¬ n ≤ K` 
-#check le_max_of_le_right 
+-- In the case `hn : ¬ n ≤ K`
+#check le_max_of_le_right
 #check lt_add_of_sub_left_lt
-#check abs_sub_abs_le_abs_sub 
+#check abs_sub_abs_le_abs_sub
 
 theorem sLim_imp_bd (hx : limₙ x a) : ∃ C, ∀ n, |x n| ≤ C :=
 by
@@ -45,17 +45,16 @@ by
 -- I is Nonempty
   have hne : I.Nonempty := by exact nonempty_range_succ
 -- J = {|x 0|, |x 1|,... ,|x K|} the image of I under |x|
-  let J := I.image |x| 
+  let J := I.image |x|
 -- Let B = max' J (exists since J is a Nonempty Finset ℕ)
   let B := J.max' (by exact Nonempty.image hne |x|)
 -- We use the bound C = max B (|a| + 1) (note this is the max of a pair of Reals)
   use max B (|a| + 1)
 -- |x n| is always ≤ either B or (|a| + 1) depending on n
   intro n
--- Do a by_cases split on `n ≤ K` 
-  by_cases hn : n ≤ K 
- -- In this case |x n| ≤ B because |x n| ∈ J 
-  · sorry 
- -- Now need to prove that n ∈ I which is true since n ≤ K 
+-- Do a by_cases split on `n ≤ K`
+  by_cases hn : n ≤ K
+ -- In this case |x n| ≤ B because |x n| ∈ J
   · sorry
-    
+ -- Now need to prove that |x n| ≤ (|a| + 1) since K ≤ n
+  · sorry
