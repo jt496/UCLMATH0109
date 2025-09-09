93cb8970f532fe22d01b490d8e8a2ced7fff388f
jt496 <john.talbot@gmail.com>
2025-09-09 11:15:09 +0100

    mathlib devcontainer

--- UCLMATH0109/.devcontainer/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf \
-  | sh -s -- --default-toolchain none  -y # Install the elan Lean version manager
-export PATH=$PATH:~/.elan/bin
-lake exe cache get!
